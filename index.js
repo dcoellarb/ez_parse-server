0ec9b8d9b19cca6a1ce2104cffb633125e0a86c0
dcoellar <dcoellar@genexususa.com>
2016-04-29 10:35:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+//var path = require('path');
+
+//var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://root:dcoburei0409@ez-mdb-0.easyruta.4855.mongodbdns.com:27000,ez-mdb-1.easyruta.4855.mongodbdns.com:27000,ez-mdb-2.easyruta.4855.mongodbdns.com:27000/easyruta?replicaSet=easyruta&ssl=false';//PROD
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://root:dcoburei0409@ezqa-mdb-0.easyruta-qa.1088.mongodbdns.com:27000/easyruta?ssl=false';//QA
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+var api = new ParseServer({
+    databaseURI: databaseUri,
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    //appId: process.env.APP_ID || 'wJjWR5KTriUoPNnDH3baAQMkWvpAhuFhNU7PsKOP', //PROD
+    //masterKey: process.env.MASTER_KEY || 'T6191e7kPbR9ksx93YEaXS2Jgyc67M6IdcdT8JXE', //PROD
+    appId: process.env.APP_ID || '2hLYHTAUJ9QXMWxQTXsOIZ2jXJLGtMauw2QN34fE', //QA
+    masterKey: process.env.MASTER_KEY || 'oqmfNH1EzA8KQM32DpWYlG2CXTJAcKs7ewVAgtUL', //QA
+    //serverURL: process.env.SERVER_URL || "http://parseserver.spayurztpa.us-east-1.elasticbeanstalk.com/parse"
+    serverURL: process.env.SERVER_URL || "http://parseserverqa.zkr8u4mwnu.us-east-1.elasticbeanstalk.com/parse"
+    //serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse'
+});
+
+/*
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
+*/
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
+
+var app = express();
+
+// Serve static assets from the /public folder
+//app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+  res.status(200).send('Make sure to star the parse-server repo on GitHub!');
+});
+
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+//app.get('/test', function(req, res) {
+//  res.sendFile(path.join(__dirname, '/public/test.html'));
+//});
+
+var port = process.env.PORT || 1337;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
